35154f6a9e37a03fe047d90387e1697e52b83028
Davi Queiroz <daviqueiroz92@gmail.com>
2019-10-31 12:24:38 -0300

    kickoff estrutura do projeto

---
+++ MyHardwareInfo/src/test/java/com/ustore/MyHardwareInfo/MyHardwareInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ustore.MyHardwareInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHardwareInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
